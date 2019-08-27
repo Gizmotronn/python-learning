33d817d8dfe358a473e0372034fe3b5c5e6dd1cb
Liam <droidology@allianceofdroids.org.au>
2019-08-27 19:37:04 +0800

    Finish Variables-Types from LearnPython.org
    Also includes some py stuff from MS vscode

--- learnpython.org/variables-types.py
+++ learnpython.org/variables-types.py
@@ -35,6 +35,22 @@ myfloat = None
 myint = None
 
 # testing code
+# Exercise for variables-types
+
+#original (need to edit):
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
+
+#edited version:    
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# testing code
 if mystring == "hello":
     print("String: %s" % mystring)
 if isinstance(myfloat, float) and myfloat == 10.0:
