a42da7ac30ad60f48d591b2744022e87a24ae10c
Liam <droidology@allianceofdroids.org.au>
2019-08-27 12:35:55 +0800

    Strings, running Python, etc

--- learnpython.org/Python - Variables & Types.md
+++ learnpython.org/Python - Variables & Types.md
@@ -37,3 +37,5 @@ print(myfloat)
 
 
 
+### Strings
+
--- learnpython.org/variables-types.py
+++ learnpython.org/variables-types.py
@@ -6,4 +6,9 @@ print(myint) # this prints the value of "myint", in this case 7
 myfloat = 7.0 # this sets the value of "myfloat" to 7.0
 print(myfloat) # this prints the value of "myfloat"
 myfloat = float(7) # this sets the calue of "myfloat" to the float of "7", making it 7.0
-print(myfloat)
+print(myfloat) # this prints the value of "myfloat" - like with line 7
+
+# in terms of line 8, you can either do line 8 or line 6 to declare a floating point variable
+
+# Running this file (also see "running python" in the root directory)
+# Right click in MS Visual Studio code, and click Run
