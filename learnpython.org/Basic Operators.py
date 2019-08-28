11a8b35053e0a335bffbd9043e6f9a325a3e6297
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-08-28 10:23:42 +0800

    Examples

--- learnpython.org/Basic Operators.py
+++ learnpython.org/Basic Operators.py
@@ -1,1 +1,5 @@
 # Check out the documentation on the Basic-Operators.md file in this folder
+
+# Examples of operators
+number = 1 + 2 # sets the value of number to 3 (1 + 2) - integer variable
+print(number) # prints the value of the variable number
--- learnpython.org/Basic-Operators.md
+++ learnpython.org/Basic-Operators.md
@@ -2,3 +2,4 @@
 
 [![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
 
+* Just as with other programming languages, basic operators (+, -, /, *) can be used in Python
