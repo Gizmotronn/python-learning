e1e9fce06f54c2a742822820847f2eb95b00cb20
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-08-28 11:34:55 +0800

    Commit

--- learnpython.org/Basic Operators.py
+++ learnpython.org/Basic Operators.py
@@ -36,3 +36,20 @@ print(all_numbers)
 print([1,2,3] * 3) # prints "1,2,3" 3 times
 
 # Exercise for "Basic Operators" from LearnPython.org
+x = object()
+y = object()
+
+# TODO: change this code
+x_list = [x,x,x,x,x,x,x,x,x,x]
+y_list = [y,y,y,y,y,y,y,y,y,y]
+big_list = ([x,y] * 10)
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
