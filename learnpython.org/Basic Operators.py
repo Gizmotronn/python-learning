6be7e7f10e7f3b9f86baa9219f748ed7615fbe84
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-08-28 10:30:42 +0800

    Types of operators in python listed in the py file

--- learnpython.org/Basic Operators.py
+++ learnpython.org/Basic Operators.py
@@ -4,5 +4,13 @@
 number = 1 + 2 # sets the value of number to 3 (1 + 2) - integer variable
 print(number) # prints the value of the variable number, in this case 3
 
+# Modulus operator
 remainder = 11 % 3 # sets the value of the variable "remainder" to the remainder of 11 divided by 3
 print(remainder)
+
+# Types of operators in Python
+# +. Addition
+# -. Subtraction
+# *. Multiplication
+# /. Division
+# %. Modulus - returns the remainder of a division
