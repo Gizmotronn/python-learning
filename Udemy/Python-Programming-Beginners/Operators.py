a43e33e580342aadc9000598bb7d4481160ede32
Liam <droidology@allianceofdroids.org.au>
2019-09-03 14:42:28 +0800

    Operators + Different Types of Operators
    From https://www.udemy.com/course/python-programming-beginners/learn/lecture/6727842#overview

---
+++ Udemy/Python-Programming-Beginners/Operators.py
@@ -0,0 +1,41 @@
+# Operators - What are Operators & Different types of Operators
+
+# What are Operators?
+# An Operator performs a function between 2 operants, for example:
+
+additionOperator = 5 + 6 # sets the value of the INTEGER VARIABLE "additionOperator" to 11, or 5+6
+
+# Types of Operators
+    # Assignment Operator
+    # Arithmetic Operator
+    # Relational Operator
+    # Logical Operator
+
+
+        # Assignment Operators  
+        # Assigning a value to a variable, for example
+assignmentOperator = "string" # the assignment operator in this case is the equals sign, on column 20
+
+
+        # Arithmetic Operators
+        # Addition (+), Multiplication (*), Subtraction (-), Division (/), Modulus (%)
+myModulus = 5 % 2 # sets the value of the variable "myModulus" to the remainder of 5 divided by 2
+
+        # Relational Operators
+        # To compare values (of variables...)
+
+
+        # Logical Operators
+        # Combination of relational operators
+        # "And" operation, + "or" operation        
+
+
+# From https://www.udemy.com/course/python-programming-beginners/learn/lecture/6727840#overview
+
+# Different Types of Operators
+
+
+
+
+
+# From https://www.udemy.com/course/python-programming-beginners/learn/lecture/6727842#overview
