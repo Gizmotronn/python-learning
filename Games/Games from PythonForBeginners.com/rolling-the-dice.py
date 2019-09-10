5dfd855060deda012cc5d056dff56ca275595b53
Liam <droidology@allianceofdroids.org.au>
2019-09-10 10:05:37 +0800

    Update rolling-the-dice.py

--- Games/Games from PythonForBeginners.com/rolling-the-dice.py
+++ Games/Games from PythonForBeginners.com/rolling-the-dice.py
@@ -12,3 +12,17 @@ but you can also add other variations to it.
 
 From https://www.pythonforbeginners.com/code-snippets-source-code/game-rolling-the-dice
 "" "
+
+import random
+min = 1
+max = 6
+
+roll_again = "yes"
+
+while roll_again == "yes" or roll_again == "y":
+    print "Rolling the dices..."
+    print "The values are...."
+    print random.randint(min, max)
+    print random.randint(min, max)
+
+    roll_again = raw_input("Roll the dices again?")
