9629c239194870ff649592215e30bf9598dc8d36
Liam <droidology@allianceofdroids.org.au>
2019-09-12 17:22:16 +0800

    Create pong.py

---
+++ Games/Pong - free code camp/pong.py
@@ -0,0 +1,11 @@
+# Simple Pong Game for Beginners
+# Youtube: https://www.youtube.com/watch?v=C6jJg9Zan7w
+# Part of Stellarios by ACORD - http://acord.tech/stellarios
+
+import turtle # module that lets you do basic graphics in Python
+
+gamewindow = turtle.Screen()
+gamewindow.title("Pong by ACORD")
+gamewindow.bgcolor("black")
+gamewindow.setup(width=800, height=600)
+w
