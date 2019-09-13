e90f822f452b0e0df211d18ace1bf403b36c3bb4
Liam <droidology@allianceofdroids.org.au>
2019-09-13 11:38:16 +0800

    Window config, fps, vscode and idle

---
+++ Games/Pong/Pong - trevor appleton/pong.py
@@ -0,0 +1,10 @@
+import pygame, sys # import pygame libraries into program, so we can access them. Also importing the sys libaries which are used to exit the game
+from pygame.locals import *
+
+# Controlling the speed (FPS) of the program
+FPS = 200 # Change this value to slow down/speed up game
+
+# Global variables used throughout the program
+# Game window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 300
