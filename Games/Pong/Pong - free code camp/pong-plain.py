1f68cc97b71b1be8ffbab0a7504288394f1bd7bd
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-09-14 16:51:10 +0800

    Finished part 2 of https://www.youtube.com/watch?v=C6jJg9Zan7w

---
+++ Games/Pong/Pong - free code camp/pong-plain.py
@@ -0,0 +1,13 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by ACORD")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+
+# Main Game Loop
+while True:
+    wn.update()
