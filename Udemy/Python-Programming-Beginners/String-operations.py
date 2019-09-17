d29866480174cf7c7757de8bd713150e4a17d576
Liam <droidology@allianceofdroids.org.au>
2019-09-17 12:40:38 +0800

    String operations from Udemy
    Problems have not yet been started as the file is not being able to be downloaded yet
    
    Link to course: https://www.udemy.com/course/python-programming-beginners/learn/lecture/6727864?start=0#overview
    
    https://github.com/acord-robotics/stellarios/tree/acord-games

---
+++ Udemy/Python-Programming-Beginners/String-operations.py
@@ -0,0 +1,21 @@
+# String Operations
+
+# string.upper()
+string = "this is a string" # sets the value of the variable "string", which is a string variable, to "this is a string"
+string.upper() # prints the value of the variable "string", however all in uppercase. E.g. 'THIS IS A STRING'
+
+# string.lower
+string.lower() # converts the value of a string to all lower case values
+
+# Replacement Operation
+string.replace("string", "string variable") # replaces the "string" in "this is a string" (the variable named string, which is a string variable) with "string variable"
+
+# Slice Operation
+string[0:4] # Uses index values to "slice" the value, which means that instead of printing "this is a string" when the "string" variable is called upon, it displays the first 5 (4) characters - "this "
+
+# Length Operation
+len(string) # prints how many index numbers/characters are in the string (13 letters + 3 spaces = 16 characters)
+
+# Exercise problems for String Operations from udemy
+# https://www.udemy.com/course/python-programming-beginners/learn/lecture/6727864?start=0#overview
+
