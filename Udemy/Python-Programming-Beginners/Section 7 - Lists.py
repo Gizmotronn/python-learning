ee64b54acc693a936ef50e8b3bb274562b9f03f0
Liam <droidology@allianceofdroids.org.au>
2019-09-18 11:22:23 +0800

    Create Section 7 - Lists.py
    Lecture link: https://www.udemy.com/course/python-programming-beginners/learn/lecture/6727846#overview

---
+++ Udemy/Python-Programming-Beginners/Section 7 - Lists.py
@@ -0,0 +1,13 @@
+# Lists
+# https://www.udemy.com/course/python-programming-beginners/learn/lecture/6727846#overview
+# listName = [object1, object3, object4]
+listName = ["object1", "object2", "object11"] # object1 has an index value of 0, object2 has an index value of 1
+listName # prints the values in the "list" with the name "listName"
+
+# Accessing values in lists
+listName[1] # prints the second object in the list "listName", in this case "object2"
+
+# Replacing/modifying lists
+listName[1] = "object7" # Replaces the value of the second value in list "listName" with "object7"
+listName # Prints the values assigned to the list "listName" to the console
+
