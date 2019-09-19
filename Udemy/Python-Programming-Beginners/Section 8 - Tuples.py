96715da2e60463cad8021479be30593b8285a7ac
Liam <droidology@allianceofdroids.org.au>
2019-09-19 21:02:41 +0800

    Create Section 8 - Tuples.py
    Course video: https://www.udemy.com/course/python-programming-beginners/learn/lecture/6727854#content

---
+++ Udemy/Python-Programming-Beginners/Section 8 - Tuples.py
@@ -0,0 +1,10 @@
+# Tuple - a collection of immutable objects (can't be changed - e.g. d.o.b). 'tuple' object does not support item assignment
+tupleName = (object1, object2) # sets the values of the objects in the tuple "tupleName" to "object1"  & "object2"
+tupleName # prints the values of the tuple "tupleName" to the console
+
+# Accessing values in a tuple
+tupleName[0] # returns "object1"
+
+# Deleting tuples
+del(tupleName) # Deletes the tuple
+tupleName = (object1, object2) # Creates tupleName again!
