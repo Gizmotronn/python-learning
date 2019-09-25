650b927e32976120c1afe8c194e310ff99e0d911
Liam <droidology@allianceofdroids.org.au>
2019-09-25 09:59:52 +0800

    Update introduction-masterclass.py

--- Udemy/Python Masterclass/introduction-masterclass.py
+++ Udemy/Python Masterclass/introduction-masterclass.py
@@ -1,1 +1,5 @@
-https://www.udemy.com/course/python-masterclass-for-beginners/learn/lecture/12851018#overview
+# https://www.udemy.com/course/python-masterclass-for-beginners/learn/lecture/12851018#overview
+
+# Q & A -- Introducing yourself
+# https://www.udemy.com/course/python-masterclass-for-beginners/learn/lecture/12851022#questions
+# Hi  there! My name is Liam Arbuckle, I'm from Australia, and I'm enrolled in this course to learn more about Python. I have a little bit of experience in Python - including lists, tuples, and a little bit of pygame, however I don't have much experience in putting those tools to real world use. This course looks like a way to fix that!
