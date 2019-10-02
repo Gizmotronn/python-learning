88df1445cbe1ecdbcd2d6c1a21346cd3f7527287
Liam <droidology@allianceofdroids.org.au>
2019-10-02 10:45:44 +0800

    Create 14-calculator-app.py
    https://www.udemy.com/course/python-masterclass-for-beginners/learn/lecture/13282934#overview

---
+++ Udemy/Python Masterclass/14-calculator-app.py
@@ -0,0 +1,14 @@
+# First Application - Calculator
+# Lecture: https://www.udemy.com/course/python-masterclass-for-beginners/learn/lecture/13282934#overview (#14)
+
+# Defining two numbers for operations
+print("Enter first number: ")
+firstNumber = input()
+print("Enter second number: ")
+secondNumber = input()
+
+# Operations
+"""
+print(firstNumber + secondNumber)
+This will not work (line 12) as both input variables are a string type
+"""
