05f8c5e8fb432c5a76540e81ec6da4ade182a969
Liam <droidology@allianceofdroids.org.au>
2019-10-08 11:26:07 +0800

    Create 17-conditionals.py
    https://www.udemy.com/course/python-masterclass-for-beginners/learn/lecture/13270028#content

---
+++ Udemy/Python Masterclass/17-conditionals.py
@@ -0,0 +1,13 @@
+# Conditionals
+# Lecture Link: https://www.udemy.com/course/python-masterclass-for-beginners/learn/lecture/13270028#content
+
+# Set-up
+variable1 = 10
+variable2 = 20
+
+# If Condition
+if variable1 < variable2: 
+    print(variable1 + " is less than " + variable2) # or print(10 is less than 20) - without the variables
+    # Anything below this line that is not indented is shown regardless of what happens with the if statement
+    # Multiple statements & statements can be put within a conditional statement
+
