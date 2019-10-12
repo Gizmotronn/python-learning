7ce631bc3e5a43323506a02d9b9a5f0a3b50c8a8
Liam <droidology@allianceofdroids.org.au>
2019-10-12 22:14:11 +0800

    Update pong-plain.py
    https://www.youtube.com/watch?time_continue=57&v=C6jJg9Zan7w
    
    Fixing problems in the original file bc the right paddle would not move, so I've done something wrong. Currently up to  step 3 - moving the paddles

--- Games/Pong/Pong - free code camp/pong-plain.py
+++ Games/Pong/Pong - free code camp/pong-plain.py
@@ -1,11 +1,39 @@
-import turtle
+import turtle # imports the "turtle" function and asset set, which draws objects on the gameboard
 
-wn = turtle.Screen()
-wn.title("Pong by ACORD")
-wn.bgcolor("black")
-wn.setup(width=800, height=600)
+# Gameboard
+wn = turtle.Screen() # Utilises Python to create a screen
+wn.title("Pong by ACORD") # Sets the title of the menu bar
+wn.bgcolor("black") # sets the background colour to black
+wn.setup(width=800, height=600) # sets the game window to 800px wide and 600px tall
 wn.tracer(0)
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) # Sets the initial value of the speed for paddle_a to 0
+paddle_a.shape("square") # this sets the dimensions to 20px*20px, which is too small (see line #15)
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1) # Resizes the paddle
+paddle_a.penup()
+paddle_a.goto(-350, 0) # sets the coords for the original position for "paddle_a"
+
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0) # Sets the initial value of the speed for paddle_b to 0
+paddle_b.shape("square") # this sets the dimensions to 20px*20px, which is too small (see line #15)
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1) # Resizes the paddle
+paddle_b.penup()
+paddle_b.goto(350, 0) # sets the coords for the original position for "paddle_b"
+
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0) # Sets the initial value of the speed for ball to 0
+ball.shape("square") # this sets the dimensions to 20px*20px, which is too small (see line #15)
+ball.color("white")
+ball.penup()
+ball.goto(0, 0) # sets the coords for the original position for "ball"
 
 
 # Main Game Loop
