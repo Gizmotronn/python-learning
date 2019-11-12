b7148aa51aef3ec76bbbce31c09a1212b1603051
Liam <droidology@allianceofdroids.org.au>
2019-11-12 10:21:10 +0800

    Update pyda-wiki module, see desc
    Also installed pip on my laptop
    See prev commits for aod

--- Udemy/Virtual Assistant/pyda-wiki.py
+++ Udemy/Virtual Assistant/pyda-wiki.py
@@ -2,4 +2,4 @@ import wikipedia
 
 while True: # this is in a loop so it can be repeated
     input = input("Ok Pyda! ")    
-    print(wikipedia.summary(input)) # For wikipedia queries
+    print(wikipedia.summary(input, sentences=2)) # For wikipedia queries
