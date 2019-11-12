5658887112e0d56b21d437afd48f824a6129ac4c
Liam <droidology@allianceofdroids.org.au>
2019-11-12 15:08:06 +0800

    PyDA/PyVA GUI started & working - see desc.
    May be renamed to PyVA (Python Virtual Assistant)
    
    Currently just need to make the gui look nicer and add the code for the funnctionaility into the gui
    
    remember to import & install {wikipedia; wolframalpha & wxPython} modules for Pip w/ Python
    
    See prev commits for AoD
    
    Part of next Stellarios package by ACoRD

--- Udemy/Virtual Assistant/pyda.py
+++ Udemy/Virtual Assistant/pyda.py
@@ -2,15 +2,20 @@ import wikipedia
 import wolframalpha
 
 while True:
-    input = input("Ok Pyda!: ")
+    pydainput = input("Ok Pyda!: ")
+
+    if pydainput == "exit":
+        break
 
     try:
         #wolframalpha
         app_id = "8A6LA2-ELRHR92Y88"
         client = wolframalpha.client(app_id)
-        result = client.query(input)
+        result = client.query(pydainput)
         answer = next(result.results).text
         print(answer)
     except:
         #wikipedia
-        print(wikipedia.summary(input))
+        print(wikipedia.summary(pydainput))
+
+        
