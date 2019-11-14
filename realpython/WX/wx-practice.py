ee70b9a43d77cb38df604a50b65cb46513573146
Liam <droidology@allianceofdroids.org.au>
2019-11-14 11:42:38 +0800

    RealPython - wxp - create app window

--- realpython/WX/wx-practice.py
+++ realpython/WX/wx-practice.py
@@ -1,2 +1,7 @@
 import wx
 # https://realpython.com/python-gui-with-wxpython/
+
+app = wx.App() # Creates & Intializes an app
+frame = wx.Frame(parent=None, title="Hello World") # app window with title/caption "Hello World"
+frame.Show() # displays frame
+app.MainLoop() # main loop for app - waits for events to occur and then executes what's in the loop
