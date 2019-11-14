3266cbc091a756fa9e23543489f952e7635f4efc
Liam <droidology@allianceofdroids.org.au>
2019-11-14 12:16:37 +0800

    wx-practice: see desc.
    Box resizer for text box in press me

--- realpython/WX/wx-practice.py
+++ realpython/WX/wx-practice.py
@@ -6,11 +6,14 @@ class MyFrame(wx.Frame):
         super().__init__(parent=None, title='Hello World')
         self.Show()
         panel = wx.Panel(self) # panels are REQUIRED for WINDOWS
+        my_sizer = wx.BoxSizer(wx.VERTICAL) # box sizer for absolute positioning & resizing windows/widgets
 
         self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5)) # text control - allows text to be created & edited
-        my_btn = wx.Button(panel, label='Press Me', pos=(5, 55)) # creates button with wxPython library, label "Press Me". Where to place the button widget (pos function)        
-
-        self.Show()
+        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+        my_btn = wx.Button(panel, label='Press Me', pos=(5, 55)) # creates button with wxPython library, label "Press Me". Where to place the button widget (pos function). This is called ABSOLUTE POSITIONING        
+        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)
+        panel.SetSizer(my_sizer)
+        self.Show() # from lines 11-16 (from self.text_ctrl to now (self.Show()), this resizes the text box in "Press Me")
 
 if __name__ == '__main__':
     app = wx.App()
