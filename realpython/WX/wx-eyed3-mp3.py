410a36cdd284fc46700bd315658f2d1d9ff5bc3e
Liam <droidology@allianceofdroids.org.au>
2019-11-15 13:53:12 +0800

    Update wx-eyed3-mp3.py
    all code inserted, no comments yet, don't fully understand all this

--- realpython/WX/wx-eyed3-mp3.py
+++ realpython/WX/wx-eyed3-mp3.py
@@ -3,3 +3,29 @@
 import wx
 import eyed3 # python tool for working with audio files
 import glob #glob 3 for python - ~~pip install glob3~~ # pathnames
+
+# Creating classes for this project
+class Mp3Panel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.row_obj_dict = {}
+
+        self..list_ctrl = wx.ListCtrl(
+            self, size=(-1, 100), 
+            style=wx.LC_REPORT | wx.BORDER_SUNKEN
+        )
+        self.list_ctrl.InsertColumn(0, 'Artist', width=140)
+        self.list_ctrl.InsertColumn(1, 'Album', width=140)
+        self.list_ctrl.InsertColumn(2, 'Title', width=200)
+        main_sizer.Add(self.list_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
+        edit_button = wx.Button(self, label='Edit')
+        edit_button.Bind(wx.EVT_BUTTON, self.on_edit)
+        main_sizer.Add(edit_button, 0, wx.ALL | wx.CENTER, 5)        
+        self.SetSizer(main_sizer)
+
+    def on_edit(self, event):
+        print('in on_edit')
+
+    def update_mp3_listing(self, folder_path):
+        print(folder_path)
