3a76e4929d5469419e097c86e54ab939ea06d9c9
Liam <droidology@allianceofdroids.org.au>
2019-11-15 12:56:01 +0800

    Update wx-eyed3-mp3.py
    imported python packages - wx, eyed3, glob

--- realpython/WX/wx-eyed3-mp3.py
+++ realpython/WX/wx-eyed3-mp3.py
@@ -1,1 +1,5 @@
 # https://realpython.com/python-gui-with-wxpython/#sizers-dynamic-sizing
+
+import wx
+import eyed3 # python tool for working with audio files
+import glob #glob 3 for python - ~~pip install glob3~~ # pathnames
