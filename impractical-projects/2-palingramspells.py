d43b9893e2c78f07b762bdb5522841247ca67c81
IrisDroidoology <droidology@allianceofdroids.org.au>
2019-12-12 12:40:22 +0800

    ACoRD: Python - IP #2 (see desc:)
    Python Impractical Projects #2 - Palingrams
    
    github/acord-robotics

---
+++ impractical-projects/2-palingramspells.py
@@ -0,0 +1,17 @@
+# Impractical Python Projects #2 (Starchfree Press)
+# Github.com/IrisDroidology/Python-Learning
+
+"""
+# Contents
+Line 11: Dictionary Files Link
+Line 15: Ideas
+"""
+
+"""
+Dictionary Files - http://www-personal.umich.edu/~jlawler/wordlist.html
+"""
+
+"""
+Ideas
+1. Some words that are created here could be found in our RPG (precursor (inscope.me) to the Stellarios game)
+"""
