43ca75bbae89bca4a37377a7c693cc08c4dc04c1
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-01-06 02:23:43 +0800

    Startings of a facebook chat bot @ ThePythonCode

--- Applets/ThePythonCode.com/wikipedia.py
+++ Applets/ThePythonCode.com/wikipedia.py
@@ -1,3 +1,4 @@
+# From https://www.thepythoncode.com/article/access-wikipedia-python
 import wikipedia # imports wikipedia pip/py module // install python and type into command prompt "pip3 install wikipedia"
 
 # Print what Wikipedia is
@@ -10,5 +11,44 @@ The Python 2 language, i.e. Python 2.7.x, was officially discontinued on January
 Python interpreters are available for many operating systems. A global community of programmers develops and maintains CPython, an open source reference implementation. A non-profit organization, the Python Software Foundation, manages and directs resources for Python and CPython development.
 """
 
+
 # Only 2 sentence summary
 print(wikipedia.summary("Python programming", sentences=2))
+
+"""
+Output:
+Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace.
+"""
+
+
+# Searching for a term in wikipedia search
+result = wikipedia.search("Neural networks")
+print(result)
+
+"""
+Output:
+['Neural network', 'Artificial neural network', 'Convolutional neural network', 'Recurrent neural network', 'Rectifier (neural networks)', 'Feedforward neural network', 'Spiking neural network', 'Dropout (neural networks)', 'Quantum neural network', 'Neural circuit']
+"""
+
+# Get the whole page for first (initial) result of search
+page = wikipedia.page(result[0])
+# Crawling the title
+title = page.title
+# Get the categories of the page
+categories = page.categories
+# Extract text after removing all HTML tags
+content = page.content # done automatically
+# Get all links from page
+links = page.links
+# References from page
+references = page.references
+# Summary from page
+summary = page.summary
+
+# Printing them all out:
+print("Page content:\n", content, "\n")
+print("Page title:", title, "\n")
+print("Categories:", categories, "\n")
+print("Links:", links, "\n")
+print("References:", references, "\n")
+print("Summary:", summary, "\n")
