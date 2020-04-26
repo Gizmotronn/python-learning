89121f38904b3e092329aad70069af3615364c12
Liam Arbuckle <allianceofdroidsla@gmail.com>
2020-04-26 18:17:37 +0800

    #22 --> Character Animation & Sprites
    @irisdroidology I am going to add my own images in later for the sprites/animations, I could possibly incorporate the Unity sprites
    
    Additions to code:
    1. Removed the ability for the player to go up or down - you need to jump to do that now:
    
    ```py
            if keys[pygame.K_UP] and y < vel:
                y -= vel
            if keys[pygame.K_DOWN] and y > screenHeight - height - vel:
                y += vel
    ```
    
    2. Loaded the images in
    
    3. Changed size of the character to reflect the img sizes
    
    4. Moved the drawing/refreshing to a function
    
    5. Changed the bg to an image
    
    More changes in `pygame1.py`
    
    I might also go over some of his other Python tutorials :)
    
    https://www.youtube.com/watch?v=UdsNBIzsmlI&list=PLzMcBGfZo4-lp3jAExUCewBfMx3UZFkh5&index=3
    
    https://www.youtube.com/playlist?list=PLzMcBGfZo4-lp3jAExUCewBfMx3UZFkh5
    
    Got an issue regarding the imgs not loading, but everything else is good :)
    
    Co-Authored-By: Liam Arbuckle <liam@acordsoftware.tech>

--- Applets/TWT/pygame1.py
+++ Applets/TWT/pygame1.py
@@ -5,12 +5,22 @@ win = pygame.display.set_mode((500, 500)) # uses the pygame module to create a w
 
 pygame.display.set_caption("Pygame") # sets the window caption
 
+# Loading images/sprites
+# This goes outside the while loop, near the top of the program
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')] # list
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')] # You COULD flip the images, but that's not what's happening here :)
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
+
+# Clock
+clock = pygame.time.Clock()
+
 screenWidth = 500
-screenHeight = 500
+screenHeight = 480
 
 # Character Attributes
 x = 50 # x position of character
-y = 50 # remembering that the coords start from the top left of the screen in pygame /#/  can change
+y = 400 # remembering that the coords start from the top left of the screen in pygame /#/  can change
 width = 40
 height = 60
 vel = 5 # speed of character --> how fast it moves
@@ -18,11 +28,32 @@ vel = 5 # speed of character --> how fast it moves
 isJump = False # indicates if character is jumping
 jumpCount = 10
 
+left = False
+right = False
+walkCount = 0 # How many steps we had
+
+def redrawGameWindow():
+    global walkCount # Global allows it to be seen anywhere
+
+    win.blit(bg, (0,0))  
+    
+    if walkCount + 1 >= 27: # frame rate 
+        walkCount = 0
+
+    if left:
+        win.blit(walkLeft[walkCount//3], char, (x,y))
+        walkCount += 1
+    elif right: 
+        win.blit(walkRight[walkCount//3], char, (x,y))      # integer remainder
+        walkCount += 1
+    else: 
+        win.blit(char, (x,y))                 
+    pygame.display.update()   
 
 # Main Game loop
 run = True
 while run == True:
-    pygame.time.delay(100) # 100 miliseconds // 0.1 seconds
+    clock.tick(27) # FPS Set to 27
 
     for event in pygame.event.get(): #makes use of the module's event get feature 
         if event.type == pygame.QUIT: # if the player closes the game
@@ -32,15 +63,22 @@ while run == True:
 
     if keys[pygame.K_LEFT] and x > vel: # left arrow key    // Solving the problem with character moving off the screen
         x -= vel
-    if keys[pygame.K_RIGHT] and x < screenWidth - width - vel:       
-        x += vel    
+        left = True
+        right = False
+    elif keys[pygame.K_RIGHT] and x < screenWidth - width - vel:       
+        x += vel 
+        right = True
+        left = False  
+    else: 
+        right = False
+        left = False
+        walkCount = 0         
     if not(isJump): # no double jump   
-        if keys[pygame.K_UP] and y < vel:    
-            y -= vel
-        if keys[pygame.K_DOWN] and y > screenHeight - height -vel:   
-            y += vel
         if keys[pygame.K_SPACE]:
             isJump = True    
+            right = False
+            left = False
+            walkCount = 0
     else: 
         if jumpCount >= -10:
             neg = 1
@@ -50,10 +88,7 @@ while run == True:
             jumpCount -= 1 # slowly move down in jump // decrement
         else: # jump has concluded
             isJump = False
-            jumpCount = 10            
-
-    win.fill((0, 0, 0)) # rgb so that the square moves rather than just accumulating    
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height)) # adding the character
-    pygame.display.update()       
+            jumpCount = 10 
+    redrawGameWindow()                       
 
 pygame.quit()       
