4ff71121a23e19e382d474c85a642d1f31a2e378
Liam Arbuckle <allianceofdroidsla@gmail.com>
2020-05-02 10:18:05 +0800

    RealPython --> Getting Started with Django (GSWD) Pt 1 - Views

--- Applets/myproject/personal_portfolio/hello_world/views.py
+++ Applets/myproject/personal_portfolio/hello_world/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def hello_world(request):
+    return render(request, 'hello_world.html', {})# Each function or class handles the logic that gets processed each time a different URL is visited.
+
+# Create your views here. 
